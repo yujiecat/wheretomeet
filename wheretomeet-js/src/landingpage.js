aa06f4ee4d9adbaebab91e40fbfdcc73093d9a80
Yujie <yuj808@gmail.com>
2021-05-20 21:03:38 -0700

    oops forgot to push these too

---
+++ wheretomeet-js/src/landingpage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+	const[title, setTitle] = React.useState('Where to Meet');
+	const[text, setText] = React.useState('A simple way to organize meetups.')
+
+	const login = (props) => {
+		setTitle(props);
+		setText();
+		document.title = 'Login';
+	}
+
+	return (<>
+	
+	<section style = {{margin: '10%'}}>
+		<h2>{title}</h2>
+		<p>{text}</p>
+		<button type="button" class="btn btn-primary btn-lg" onClick = {() => login('Login')} >Sign Up/Sign In</button>
+		<button type="button" class="btn btn-secondary btn-lg" disabled>Create Group</button>
+	</section>
+
+	</>)
+
+}
+
+export default LandingPage;
